ddbe63cb047e25822a57514ed4d06c33d9f190ca
CarsonKnapp <knappcd@masters.edu>
2024-12-09 16:37:00 -0800

    Final Commit

--- backend/lib/persistent-database.js
+++ backend/lib/persistent-database.js
@@ -9,7 +9,7 @@ const initialData = {
       lname: "Sinclair",
       quote: "I've been interested in coding for a while now, but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams and so excited about the future.",
       portrait_img:
-        "https://media.istockphoto.com/id/2135643049/photo/waist-up-shot-of-a-handsome-hispanic-latino-carefree-black-male-looking-at-the-camera-with.webp?a=1&b=1&s=612x612&w=0&k=20&c=9UhZSA9t22Mu5gXF9Y3yQlPNA9nXbbKaS78ThhhQXgM=",
+        "https://m.media-amazon.com/images/M/MV5BNTA4MDgzMTc1Nl5BMl5BanBnXkFtZTcwNjQyMjIwNQ@@._V1_QL75_UY148_CR7,0,100,148_.jpg",
     },
     {
       id: "c5431",
@@ -17,7 +17,7 @@ const initialData = {
       lname: "Tarkpor",
       quote: "If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.",
       portrait_img:
-        "https://media.istockphoto.com/id/2135643049/photo/waist-up-shot-of-a-handsome-hispanic-latino-carefree-black-male-looking-at-the-camera-with.webp?a=1&b=1&s=612x612&w=0&k=20&c=9UhZSA9t22Mu5gXF9Y3yQlPNA9nXbbKaS78ThhhQXgM=",
+        "https://m.media-amazon.com/images/M/MV5BMTY4Mjg0NjIxOV5BMl5BanBnXkFtZTcwMTM2NTI3MQ@@._V1_QL75_UX100_CR0,1,100,148_.jpg",
     },
     {
       id: "d6790",
@@ -25,7 +25,7 @@ const initialData = {
       lname: "Cunningham",
       quote: "This course has helped me deploy a full-stack application. I now have a job at a top tech company. I am so grateful for the knowledge gained.",
       portrait_img:
-        "https://media.istockphoto.com/id/2135643049/photo/waist-up-shot-of-a-handsome-hispanic-latino-carefree-black-male-looking-at-the-camera-with.webp?a=1&b=1&s=612x612&w=0&k=20&c=9UhZSA9t22Mu5gXF9Y3yQlPNA9nXbbKaS78ThhhQXgM=",
+        "https://m.media-amazon.com/images/M/MV5BMTM0MTc0Njk2OF5BMl5BanBnXkFtZTcwMTIxMTAzNQ@@._V1_QL75_UY148_CR6,0,100,148_.jpg",
     },
   ],
 }
